b20065b665132c3c8d24053ce3e652418bf547d5
-v <wtrzos@gmail.com>
2018-07-03 22:11:57 +0200

    widok vehicle

--- src/main/java/pl/coderslab/controller/VehicleView.java
+++ src/main/java/pl/coderslab/controller/VehicleView.java
@@ -6,14 +6,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "VehicleView")
 public class VehicleView extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF8");
+        response.setContentType("text/html, charset:utf-8");
+
+        PrintWriter pisak = response.getWriter();
+        String customer = getInitParameter("custid");
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+//todo redirect do wyboru klienta
     }
 }
